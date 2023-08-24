94be16e490555f3fe4b11bae2b1d1ccd59765674
Dinesh-4 <dinesh46.mv@gmail.com>
2023-08-24 16:36:24 +0530

    Navigation Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <span>Real Estate Website</span>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
