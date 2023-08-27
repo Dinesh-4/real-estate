ffadb5e6e831602e76529daf4c34bcb29d6f8ec0
Dinesh-4 <dinesh46.mv@gmail.com>
2023-08-27 12:35:46 +0530

    Clients Companies Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import './App.css'
+import Companies from "./components/Companies/Companies";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
     </div>
   );
 }
